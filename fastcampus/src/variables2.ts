60c70c1b12f0841cf0ca166e16c6b563254f7bda
NANGGI <mqsora@naver.com>
2021-04-28 00:49:14 +0900

    Add files via upload

---
+++ fastcampus/src/variables2.ts
@@ -0,0 +1,30 @@
+// array type annotation
+let nameList: string[];
+
+// error
+nameList = [1, 2, 3];
+nameList.push(4);
+
+nameList = ["1", "2", "3"];
+nameList.push("4");
+
+// array type annotation2
+let tuple: [string, number];
+
+// error
+tuple = [1, "2"];
+
+tuple = ["1", 2];
+
+// object type annotation
+let userList: { name: string, score: number };
+
+// error
+userList = {
+   age: '123'
+}
+
+userList = {
+   name: 'sample',
+   score: 100
+}
