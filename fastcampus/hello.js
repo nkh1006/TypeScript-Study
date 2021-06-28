df567707e66371b5b69efe1f7661f5b87a00ef5c
NANGGI <mqsora@naver.com>
2021-06-28 14:57:56 +0900

    Add files via upload

---
+++ fastcampus/hello.js
@@ -0,0 +1,9 @@
+var hello = "hello";
+var hello2 = "hello2";
+// es6에 추가된 promise는 es5버전으로 컴파일 했을경우 찾을수 없음
+var timeoutPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve("1 sec");
+    }, 1000);
+});
+timeoutPromise.then(console.log);
