00458e8513420421fc6a7257ed1db0b48116c95d
NANGGI <mqsora@naver.com>
2021-07-08 00:05:39 +0900

    Add files via upload

---
+++ tutorial_for_beginners/main.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello World';
+console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = 'Codevolution';
+var isBeginner = true;
+var total = 0;
+var name = 'Your Name';
+var sentence = "My Name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
---
+++ tutorial_for_beginners/main.ts
@@ -0,0 +1,47 @@
+export {}
+
+let message = 'Hello World';
+console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'Codevolution';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Your Name';
+
+let sentence: string = `My Name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ['Chris', 22];
+
+enum Color {Red, Green, Blue};
+let c: Color = Color.Green;
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Name !';
+
+let myVariable: any = 10;
+
+console.log(myVariable.name);
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
