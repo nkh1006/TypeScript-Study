4fbc8409b9ac68b67aa35ef9c3e40506282dd20f
NANGGI <mqsora@naver.com>
2021-07-11 22:30:19 +0900

    Add files via upload

--- tutorial_for_beginners/main.ts
+++ tutorial_for_beginners/main.ts
@@ -1,5 +1,4 @@
 export {}
-
 let message = 'Hello World';
 console.log(message);
 
@@ -11,15 +10,16 @@ const title = 'Codevolution';
 
 let isBeginner: boolean = true;
 let total: number = 0;
-let name: string = 'Your Name';
+let name: string = 'Vishwas';
 
-let sentence: string = `My Name is ${name}
-I am a beginner in Typescript`;
+// error 
+// name = true;
 
+let sentence: string = `My name is ${name}`;
 console.log(sentence);
 
-let n: null = null;
-let u: undefined = undefined;
+const n: null = null;
+const u: undefined = undefined;
 
 let isNew: boolean = null;
 let myName: string = undefined;
@@ -29,19 +29,13 @@ let list2: Array<number> = [1, 2, 3];
 
 let person1: [string, number] = ['Chris', 22];
 
-enum Color {Red, Green, Blue};
+enum Color = {Red, Green, Blue};
+
 let c: Color = Color.Green;
+
+// 2
+console.log(c);
 
 let randomValue: any = 10;
 randomValue = true;
-randomValue = 'Name !';
-
-let myVariable: any = 10;
-
-console.log(myVariable.name);
-
-let a;
-a = 10;
-a = true;
-
-let b = 20;
+randomValue = 15;
