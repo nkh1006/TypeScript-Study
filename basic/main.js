0f544cf1ac31cff8e199f7eb3800c0dbc6775864
NANGGI <mqsora@naver.com>
2021-07-26 15:30:17 +0900

    Add files via upload

---
+++ basic/main.js
@@ -0,0 +1,7 @@
+// compiling and variables
+var bob = "hey I'am Bob !";
+console.log(bob);
+// functions
+// unions
+// casting
+// any
---
+++ basic/main.ts
@@ -0,0 +1,31 @@
+// compiling and variables
+
+const bob = "hey I'am Bob !";
+const a = 5.321;
+interface MyObj {
+    x: number;
+    y: number;
+};
+const obj: MyObj = {
+    x: 5,
+    y: 7
+};
+const bob2 = () => {};
+console.log(bob);
+
+// functions
+
+type AddFunc = (x: number, y: number) => string;
+
+const add = (x, y) => x + y;
+
+// unions
+
+interface Dog {                     
+
+    
+}
+
+// casting
+
+// any
