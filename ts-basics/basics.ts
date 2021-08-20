73ba752c3cc339f01aa6448aefa9648fc1ce9741
NANGGI <mqsora@naver.com>
2021-08-20 15:24:52 +0900

    Add files via upload

---
+++ ts-basics/basics.ts
@@ -0,0 +1,8 @@
+let userName = "Jack";
+let hasLoggedIn: boolean = true;
+let myNumber: number = 10;
+let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split("");
+const myValues: Array<number> = [1, 2, 3];
+
