9cdee8f8e1e701bae230116bb0201f61c09f2d34
NANGGI <mqsora@naver.com>
2021-08-23 15:48:13 +0900

    Add files via upload

--- poiemaweb/#1_typescript.js
+++ poiemaweb/#1_typescript.js
@@ -1,12 +1,19 @@
-// 자바스크립에서는 정적타입의 선언이 되지 않는다.
-
+// 타입스크립트에서는 정적 타입의 변수를 선언 할수 있다.
+// 때문에 컴파일단계에서의 예기치 못한 오류나 변수를 방지 할 수있다.
 function sum(a, b) {
-  return a + b;
+    return a + b;
 }
-
-// 따라서 아래와 같은 상황에서도 아무런 에러가 없다.
 function sum2(a, b) {
-  return a + b;
+    return a + b;
 }
-
-sum2('x', 'y'); // 'xy'
+sum2("x", "y");
+// error TS2345: Argument of type '"x"' is not assignable to parameter of type 'number'.
+// 함수선언식
+function multiply1(x, y) {
+    return x * y;
+}
+// 함수표현식
+var multiply2 = function (x, y) { return x * y; };
+console.log(multiply1(10, 2));
+console.log(multiply2(10, 3));
+console.log(multiply1(true, 1)); // error TS2345: Argument of type 'true' is not assignable to parameter of type 'number'.
--- poiemaweb/#1_typescript.ts
+++ poiemaweb/#1_typescript.ts
@@ -9,5 +9,19 @@ function sum2(a: number, b: number) {
   return a + b;
 }
 
-sum2('x', 'y');
+sum2("x", "y");
 // error TS2345: Argument of type '"x"' is not assignable to parameter of type 'number'.
+
+
+// 함수선언식
+function multiply1(x: number, y: number): number {
+  return x * y;
+}
+
+// 함수표현식
+const multiply2 = (x: number, y: number): number => x * y;
+
+console.log(multiply1(10, 2));
+console.log(multiply2(10, 3));
+
+console.log(multiply1(true, 1)); // error TS2345: Argument of type 'true' is not assignable to parameter of type 'number'.
