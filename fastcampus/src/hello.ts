8fea9d7bc4448d9a2d060e9cc434aaad491c5f0f
NANGGI <mqsora@naver.com>
2021-11-03 23:53:37 +0900

    Add files via upload

--- fastcampus/src/hello.ts
+++ fastcampus/src/hello.ts
@@ -1,15 +1,4 @@
-import add from './util';
+import add from "./util";
 
-var hello = "hello";
-let hello2 = "hello2";
-
-// es6에 추가된 promise는 es5버전으로 컴파일 했을경우 찾을수 없음
-let timeoutPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("1 sec");
-    }, 1000);
-});
-timeoutPromise.then(console.log);
-
-const returnResult = add(1,2);
-console.log(returnResult);
+const value = add(1, 2);
+console.log(value);
