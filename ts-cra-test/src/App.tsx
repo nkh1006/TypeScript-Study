82c9bd2db9e9790c51bf02e59daf5551a8f76082
NANGGI <mqsora@naver.com>
2021-12-13 00:05:12 +0900

    Add files via upload

--- ts-cra-test/src/App.tsx
+++ ts-cra-test/src/App.tsx
@@ -1,66 +1,94 @@
-import React, { ReactElement, ReactNode, useState, FC } from 'react';
-import './App.css';
+import React, { Children, ReactElement, ReactNode, useState } from 'react'
+import "./App.css";
 
-// const HeadingFC: React.FC<{title: string}> = ({ title }) => <h1>{title}</h1>;
-
-// Conventional props
-function Heading({ title }: { title: string;}) {
-  return (
-    <h1>{title}</h1>
-  )
+const HeadingFC: React.FC<{ title: string }> = ({ title }) => { 
+  return <h1>{title}</h1>;
 }
 
-function HeadingWithContent({ children }: { children: ReactNode}): ReactElement | null {
-  return (
-    <h1>{children}</h1>
-  )
+// Conventional props
+const Heading = ({ title }: { title: string }) => {
+  return <h1>{title}</h1>;
+}
+
+const HeadingWithContent = ({ children }: { children: ReactNode }): ReactElement => {
+  return <h1>{children}</h1>;
 }
 
 // defaultProps
 const defaultContainerProps = {
-  heading: <strong>My Heading</strong>,
-};
-
-type ContainerProps = { heading: ReactNode, children: ReactNode} & typeof defaultContainerProps;
-
-function Container({ heading, children }: ContainerProps): ReactElement {
+  heading: <strong>My Heading</strong>
+}
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps;
+const Container = ({ heading, children }: ContainerProps): ReactElement => {
   return (
     <div>
-      <h1>{heading}</h1>{children}
+      <h1>{heading}</h1>
+      {children}
     </div>
-  )
+  );
 }
 
 Container.defaultProps = defaultContainerProps;
 
-// Functional Props
-function TextWithNumber({ header, children }: { header?: (num: number) => ReactNode; children: (num: number) => ReactNode; }) {
-  const [state, setState] = useState<number>(1);
+// Functional props
+function TextWithNumber({
+  header,
+  children 
+}: {
+  header: (num: number) => ReactNode;
+  children: (num: number) => ReactNode
+}) {
+  const [state, stateSet] = useState<number>(1);
 
   return (
     <div>
-      {header && <h2>{header?.(state)}</h2>}
-      <div>{children(state)}</div>
+      <h2>{header?.(state)}</h2>
       <div>
-        <button onClick={() => setState(state + 1)}>Add</button>
+        {children(state)}
+      </div>
+      <div>
+        <button onClick={() => stateSet(state + 1)}>Add</button>
       </div>
     </div>
   )
 }
 
-function App() {
+// List
+function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[],
+  render: (item: ListItem) => ReactNode
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          {render(item)}
+        </li>
+      ))}
+    </ul>
+  )
+};
+
+const App = () => {
   return (
     <div>
-      <Heading title="Hello there"></Heading>
+      <Heading title="Hello"></Heading>
       <HeadingWithContent>
-        Hi !
+        <strong>hi!</strong>
       </HeadingWithContent>
       <Container>
         Foo
       </Container>
-      <TextWithNumber>
-        {(num: number) => <div>Today's number number is {num}</div>}
+      <TextWithNumber header={(num: number) => <span>Header {num}</span>}>
+        {(num: number) => <div>Today's number is {num}</div>}
       </TextWithNumber>
+      <List 
+        items={["Jack", "Sadie", "oso"]} 
+        render={(item: string) => <div>{item.toLowerCase()}</div>
+      }></List>
     </div>
   );
 }
