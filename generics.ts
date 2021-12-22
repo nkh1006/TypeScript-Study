0696da02b9202614d57398d4888230402b3f99c8
NANGGI <mqsora@naver.com>
2021-12-22 17:24:21 +0900

    Add files via upload

---
+++ generics.ts
@@ -0,0 +1,11 @@
+function simpleState<T>(initial: T): [() => T, (v: T) => void] {
+  let val: T = initial;
+  return [
+    () => val,
+    (v: T) => {
+      val = v;
+    }
+  ]
+}
+
+const [st1getter, st1setter] = simpleState(10);
